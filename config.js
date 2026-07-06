d0269342c86081a31b1ee80e47e8ec0a46ebccc6
mitiospetr-gif <mitiospetr@gmail.com>
2026-07-06 19:47:19 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-﻿// Конфигурация Supabase
-const SUPABASE_CONFIG = {
-    url: 'https://jqpuwagaogczjrffzrrm.supabase.co',
-    anonKey: 'sb_publishable_agM52VWOsqUR_GpSEzyVxA_cl6isEst'
-};
-
-window.SUPABASE_CONFIG = SUPABASE_CONFIG;
