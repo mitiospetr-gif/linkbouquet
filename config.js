4bd19c4d17276ca2d53d9124a045b8f5b4c20cde
mitiospetr-gif <mitiospetr@gmail.com>
2026-07-06 19:05:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+﻿// Конфигурация Supabase
+const SUPABASE_CONFIG = {
+    url: 'https://jqpuwagaogczjrffzrrm.supabase.co',
+    anonKey: 'sb_publishable_agM52VWOsqUR_GpSEzyVxA_cl6isEst'
+};
+
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
