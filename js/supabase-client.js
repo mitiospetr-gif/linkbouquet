83eecd65f896bd18d5739432523f07afb20b8fc2
mitiospetr-gif <mitiospetr@gmail.com>
2026-07-06 19:19:51 +0700

    Delete js/supabase-client.js

--- js/supabase-client.js
+++
@@ -1,9 +0,0 @@
-﻿// Инициализация Supabase клиента
-const { createClient } = supabase;
-
-const supabaseClient = createClient(
-    window.SUPABASE_CONFIG.url,
-    window.SUPABASE_CONFIG.anonKey
-);
-
-window.supabaseClient = supabaseClient;
